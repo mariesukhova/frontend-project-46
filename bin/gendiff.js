2b803a64f835c0cc020f9c5a4bf6793493aefe80
mariesukhova <mariesukhova@gmail.com>
2022-12-13 17:41:13 +0300

    step 2 - added output info

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .version('output the version number')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
