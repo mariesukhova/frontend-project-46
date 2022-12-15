ff54ad03844a367687e70b31a095a258062c7075
mariesukhova <mariesukhova@gmail.com>
2022-12-15 18:59:15 +0300

    step 3 - added genDiff logic

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
 import { Command } from 'commander';
 
 const program = new Command();
@@ -6,7 +9,33 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('output the version number')
-  .argument('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const firstFile = fs.readFileSync(path.resolve(filepath1));
+    const secondFile = fs.readFileSync(path.resolve(filepath2));
 
+    const firstFileParsed = JSON.parse(firstFile);
+    const secondFileParsed = JSON.parse(secondFile);
+
+    const keys1 = Object.keys(firstFileParsed);
+    const keys2 = Object.keys(secondFileParsed);
+    const mergedKeys = _.union(keys1, keys2).sort();
+
+    const result = mergedKeys.map((key) => {
+      if (firstFileParsed[key] === secondFileParsed[key]) {
+        return `  ${key} : ${firstFileParsed[key]}`;
+      }
+      if (!(key in firstFileParsed) && key in secondFileParsed) {
+        return `+ ${key} : ${secondFileParsed[key]}`;
+      }
+      if (key in firstFileParsed && !(key in secondFileParsed)) {
+        return `- ${key} : ${firstFileParsed[key]}`;
+      }
+      return `- ${key} : ${firstFileParsed[key]} \n + ${key} : ${secondFileParsed[key]}`;
+    });
+    console.log(`{\n ${result.join('\n ')}\n}`);
+  });
 program.parse();
+
+export default program;
