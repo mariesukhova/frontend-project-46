16939c99d6b1d01db86c54ec08120e43a500e538
Maria Sukhova <mariesukhova@gmail.com>
2023-04-10 23:00:29 +0300

    [step-4] Add tests for same and empty files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,20 +6,45 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
-const filePath1 = getFixturePath('file1.json');
-const filePath2 = getFixturePath('file2.json');
-
-test('test1', async () => {
+test('different files', async () => {
+  const filePath1 = getFixturePath('file1.json');
+  const filePath2 = getFixturePath('file2.json');
   const result = gendiff(filePath1, filePath2);
 
   const diff = `{
  - follow : false
    host : hexlet.io
  - proxy : 123.234.53.22
- - timeout : 50 
+ - timeout : 50
  + timeout : 20
  + verbose : true
-}`
+}`;
+
+  expect(result).toEqual(diff);
+});
+
+test('empty files', async () => {
+  const filePath1 = getFixturePath('file3.json');
+  const filePath2 = getFixturePath('file4.json');
+  const result = gendiff(filePath1, filePath2);
+
+  const diff = `{
+}`;
+
+  expect(result).toEqual(diff);
+});
+
+test('same files', async () => {
+  const filePath1 = getFixturePath('file1.json');
+  const filePath2 = getFixturePath('file5.json');
+  const result = gendiff(filePath1, filePath2);
+
+  const diff = `{
+   follow : false
+   host : hexlet.io
+   proxy : 123.234.53.22
+   timeout : 50
+}`;
 
   expect(result).toEqual(diff);
 });
