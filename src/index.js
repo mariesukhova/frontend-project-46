4038657d54954abad10b8a6a3f2d8d3fc0842929
Maria Sukhova <mariesukhova@gmail.com>
2023-04-10 21:13:39 +0300

    [step-4] Move gendiff func in src

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-import gendiff from '../bin/gendiff.js';
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+const gendiff = (filepath1, filepath2) => {
+  const firstFile = fs.readFileSync(path.resolve(filepath1));
+  const secondFile = fs.readFileSync(path.resolve(filepath2));
+
+  const firstFileParsed = JSON.parse(firstFile);
+  const secondFileParsed = JSON.parse(secondFile);
+
+  const keys1 = Object.keys(firstFileParsed);
+  const keys2 = Object.keys(secondFileParsed);
+  const mergedKeys = _.union(keys1, keys2).sort();
+
+  const result = mergedKeys.map((key) => {
+    if (firstFileParsed[key] === secondFileParsed[key]) {
+      return `  ${key} : ${firstFileParsed[key]}`;
+    }
+    if (!(key in firstFileParsed) && key in secondFileParsed) {
+      return `+ ${key} : ${secondFileParsed[key]}`;
+    }
+    if (key in firstFileParsed && !(key in secondFileParsed)) {
+      return `- ${key} : ${firstFileParsed[key]}`;
+    }
+    return `- ${key} : ${firstFileParsed[key]} \n + ${key} : ${secondFileParsed[key]}`;
+  });
+  return `{\n ${result.join('\n ')}\n}`;
+};
 
 export default gendiff;
