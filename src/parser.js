dbf2a15daa39ba27cc1a14ec614fa454cb623b72
Maria Sukhova <mariesukhova@gmail.com>
2023-04-11 15:55:24 +0300

    [step-5] Add parser

---
+++ src/parser.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import yaml from 'js-yaml';
+
+const parser = (file, type) => {
+  switch (type) {
+    case '.yml':
+    case '.yaml':
+      return yaml.load(file);
+    case '.json':
+    default:
+      return JSON.parse(file);
+  }
+};
+
+export default parser;
