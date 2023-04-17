6a76f4223651df2bcbbe3092834501f8af81d3d7
Maria Sukhova <mariesukhova@gmail.com>
2023-04-17 19:13:44 +0300

    [step-6] Fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,93 +1,49 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
 import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
+const getContent = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
 test('different json files', async () => {
   const filePath1 = getFixturePath('file1.json');
   const filePath2 = getFixturePath('file2.json');
-  const result = gendiff(filePath1, filePath2);
+  const resultPath = getFixturePath('resultStylish.txt');
+  const result = getContent(resultPath);
+  const diff = gendiff(filePath1, filePath2);
 
-  const diff = `{
- - follow : false
-   host : hexlet.io
- - proxy : 123.234.53.22
- - timeout : 50
- + timeout : 20
- + verbose : true
-}`;
-
-  expect(result).toEqual(diff);
+  expect(diff).toEqual(result);
 });
 
 test('empty json files', async () => {
   const filePath1 = getFixturePath('file3.json');
   const filePath2 = getFixturePath('file4.json');
-  const result = gendiff(filePath1, filePath2);
+  const resultPath = getFixturePath('resultEmpty.txt');
+  const result = getContent(resultPath);
+  const diff = gendiff(filePath1, filePath2);
 
-  const diff = `{
-}`;
-
-  expect(result).toEqual(diff);
-});
-
-test('same json files', async () => {
-  const filePath1 = getFixturePath('file1.json');
-  const filePath2 = getFixturePath('file5.json');
-  const result = gendiff(filePath1, filePath2);
-
-  const diff = `{
-   follow : false
-   host : hexlet.io
-   proxy : 123.234.53.22
-   timeout : 50
-}`;
-
-  expect(result).toEqual(diff);
+  expect(diff).toEqual(result);
 });
 
 test('different yaml files', async () => {
   const filePath1 = getFixturePath('file1.yaml');
   const filePath2 = getFixturePath('file2.yaml');
-  const result = gendiff(filePath1, filePath2);
+  const resultPath = getFixturePath('resultStylish.txt');
+  const result = getContent(resultPath);
+  const diff = gendiff(filePath1, filePath2);
 
-  const diff = `{
- - follow : false
-   host : hexlet.io
- - proxy : 123.234.53.22
- - timeout : 50
- + timeout : 20
- + verbose : true
-}`;
-
-  expect(result).toEqual(diff);
+  expect(diff).toEqual(result);
 });
 
 test('empty yml files', async () => {
   const filePath1 = getFixturePath('file3.yml');
   const filePath2 = getFixturePath('file4.yml');
-  const result = gendiff(filePath1, filePath2);
+  const resultPath = getFixturePath('resultEmpty.txt');
+  const result = getContent(resultPath);
+  const diff = gendiff(filePath1, filePath2);
 
-  const diff = `{
-}`;
-
-  expect(result).toEqual(diff);
-});
-
-test('same yaml files', async () => {
-  const filePath1 = getFixturePath('file1.yaml');
-  const filePath2 = getFixturePath('file5.yml');
-  const result = gendiff(filePath1, filePath2);
-
-  const diff = `{
-   follow : false
-   host : hexlet.io
-   proxy : 123.234.53.22
-   timeout : 50
-}`;
-
-  expect(result).toEqual(diff);
+  expect(diff).toEqual(result);
 });
