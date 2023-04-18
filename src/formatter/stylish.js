9b1228240b0785316815c9e04a668a9ca9f4e861
Maria Sukhova <mariesukhova@gmail.com>
2023-04-18 23:29:52 +0300

    [step-7] Fix stylish formatter

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -1,29 +1,47 @@
+import _ from 'lodash';
+
+const getIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(spacesCount * depth - 2);
+
+const getContent = (value, depth) => {
+  if (!_.isObject(value)) {
+    return value;
+  }
+  const keys = Object.keys(value);
+  const result = keys.map((key) => `${getIndent(depth + 1)}  ${key}: ${getContent(value[key], depth + 1)}`);
+  return `{\n${result.join('\n')}\n${getIndent(depth)}  }`;
+};
+
 export default function getStylish(diff) {
-  const iter = (data, depth, replacer = ' ', spacesCount = 4) => {
-    // console.log(data)
-    const result = data.map((obj) => {
-      const indent = spacesCount * depth - 2;
-      if (obj.parent) {
-        const { key, value } = obj.parent;
-        switch (obj.parent.type) {
+  const iter = (data, depth) => {
+    const result = data.map((element) => {
+      const {
+        key, value, value1, value2, isParent, type,
+      } = element;
+
+      if (isParent) {
+        switch (type) {
           case 'added':
-            return `\n${replacer.repeat(indent)}+ ${key}: {${iter(value, (depth + 1))}\n${replacer.repeat(indent + 2)}}`;
+            return `\n${getIndent(depth)}+ ${key}: {${iter(value, (depth + 1))}\n${getIndent(depth)}}`;
           case 'deleted':
-            return `\n${replacer.repeat(indent)}- ${key}: {${iter(value, (depth + 1))}\n${replacer.repeat(indent + 2)}}`;
+            return `\n${getIndent(depth)}- ${key}: {${iter(value, (depth + 1))}\n${getIndent(depth)}}`;
+          case 'different value':
+            if (Array.isArray(value1)) {
+              return `\n${getIndent(depth)}- ${key}: {${iter(value1, (depth + 1))}\n${getIndent(depth)}  }\n${getIndent(depth)}+ ${key}: ${value2}`;
+            }
+            return `\n${getIndent(depth)}- ${key}: ${value1}\n${getIndent(depth)}+ ${key}: {${iter(value2, (depth + 1))}\n${getIndent(depth)}  }`;
           default:
-            return `\n${replacer.repeat(indent)}  ${key}: {${iter(value, (depth + 1))}\n${replacer.repeat(indent + 2)}}`;
+            return `\n${getIndent(depth)}  ${key}: {${iter(value, (depth + 1))}\n${getIndent(depth)}  }`;
         }
       }
-      const { key, value } = obj.child;
-      switch (obj.child.type) {
+      switch (type) {
         case 'added':
-          return `\n${replacer.repeat(indent)}+ ${key}: ${value}`;
+          return `\n${getIndent(depth)}+ ${key}: ${getContent(value, depth)}`;
         case 'deleted':
-          return `\n${replacer.repeat(indent)}- ${key}: ${value}`;
+          return `\n${getIndent(depth)}- ${key}: ${getContent(value, depth)}`;
         case 'different value':
-          return `\n${replacer.repeat(indent)}- ${key}: ${obj.child.value1}\n${replacer.repeat(indent)}+ ${key}: ${obj.child.value2}`;
+          return `\n${getIndent(depth)}- ${key}: ${getContent(value1, depth)}\n${getIndent(depth)}+ ${key}: ${getContent(value2, depth)}`;
         default:
-          return `\n${replacer.repeat(indent)}  ${key}: ${value}`;
+          return `\n${getIndent(depth)}  ${key}: ${getContent(value, depth)}`;
       }
     });
     return result.join('');
