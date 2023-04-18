95843f85925c7fdf51c08b140f08b0995973d039
Maria Sukhova <mariesukhova@gmail.com>
2023-04-18 23:27:36 +0300

    [step-7] Rename format parser

--- src/formatter/index.js
+++ src/formatter/index.js
@@ -1,11 +1,14 @@
 import getStylish from './stylish.js';
+import getPlain from './plain.js';
 
 const getFormat = (data, format) => {
   switch (format) {
     case 'stylish':
       return getStylish(data);
+    case 'plain':
+      return getPlain(data);
     default:
-      return [];
+      return getStylish(data);
   }
 };
 
