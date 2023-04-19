321a1e9b2b97f59e87aa4ccfa567f9dafe61d9ec
Maria Sukhova <mariesukhova@gmail.com>
2023-04-19 14:48:38 +0300

    [step-9] Add export

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,10 +8,12 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('output the version number')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .arguments('<filepath1> <filepath2>')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .action((filepath1, filepath2, options) => {
     console.log(gendiff(filepath1, filepath2, options.format));
   });
+
 program.parse();
 
 export default program;
