f24c064250464dfc16ed38dbf77e20b4cdedaa4d
Maria Sukhova <mariesukhova@gmail.com>
2023-04-19 22:29:30 +0300

    [step-9] Add default format test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,7 +13,7 @@ test('different json files with stylish format', async () => {
   const filePath2 = getFixturePath('file2.json');
   const resultPath = getFixturePath('resultStylish.txt');
   const result = getContent(resultPath);
-  const diff = gendiff(filePath1, filePath2);
+  const diff = gendiff(filePath1, filePath2, 'stylish');
 
   expect(diff).toEqual(result);
 });
@@ -23,7 +23,7 @@ test('different yml files with stylish format', async () => {
   const filePath2 = getFixturePath('file2.yaml');
   const resultPath = getFixturePath('resultStylish.txt');
   const result = getContent(resultPath);
-  const diff = gendiff(filePath1, filePath2);
+  const diff = gendiff(filePath1, filePath2, 'stylish');
 
   expect(diff).toEqual(result);
 });
@@ -64,6 +64,26 @@ test('different yaml files with JSON format', async () => {
   const resultPath = getFixturePath('resultJSON.txt');
   const result = getContent(resultPath);
   const diff = gendiff(filePath1, filePath2, 'json');
+
+  expect(diff).toEqual(result);
+});
+
+test('different json files with default format', async () => {
+  const filePath1 = getFixturePath('file1.json');
+  const filePath2 = getFixturePath('file2.json');
+  const resultPath = getFixturePath('resultStylish.txt');
+  const result = getContent(resultPath);
+  const diff = gendiff(filePath1, filePath2);
+
+  expect(diff).toEqual(result);
+});
+
+test('different yml files with default format', async () => {
+  const filePath1 = getFixturePath('file1.yaml');
+  const filePath2 = getFixturePath('file2.yaml');
+  const resultPath = getFixturePath('resultStylish.txt');
+  const result = getContent(resultPath);
+  const diff = gendiff(filePath1, filePath2);
 
   expect(diff).toEqual(result);
 });
