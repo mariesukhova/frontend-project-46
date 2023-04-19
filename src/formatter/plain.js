47bcd4d0c9ce8205d9da4c4e9a19148175a3a943
Maria Sukhova <mariesukhova@gmail.com>
2023-04-19 22:31:15 +0300

    [step-9] Rewrite formatters

--- src/formatter/plain.js
+++ src/formatter/plain.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const formatValue = (val) => {
+function formatValue(val) {
   if (_.isObject(val)) {
     return '[complex value]';
   }
@@ -8,28 +8,33 @@ const formatValue = (val) => {
     return `'${val}'`;
   }
   return `${val}`;
-};
+}
+
+function createString(element, newPath, iter) {
+  const {
+    value, type, isParent, value1, value2,
+  } = element;
+
+  if (isParent) {
+    return iter(value, newPath);
+  }
+  if (type === 'added') {
+    return `Property '${newPath}' was added with value: ${formatValue(value)}`;
+  }
+  if (type === 'deleted') {
+    return `Property '${newPath}' was removed`;
+  }
+  if (type === 'different value') {
+    return `Property '${newPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`;
+  }
+  return null;
+}
 
 export default function getPlain(diff) {
   const iter = (data, path = '') => {
-    const result = data.map(({
-      key, value, type, isParent, value1, value2,
-    }) => {
-      const newPath = !path ? key : `${path}.${key}`;
-
-      if (type === 'added') {
-        return `Property '${newPath}' was added with value: ${formatValue(value)}`;
-      }
-      if (type === 'deleted') {
-        return `Property '${newPath}' was removed`;
-      }
-      if (type === 'different value') {
-        return `Property '${newPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`;
-      }
-      if (isParent) {
-        return iter(value, newPath);
-      }
-      return null;
+    const result = data.map((element) => {
+      const newPath = !path ? element.key : `${path}.${element.key}`;
+      return createString(element, newPath, iter);
     });
 
     return result.filter((i) => i !== null).join('\n');
--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -2,47 +2,65 @@ import _ from 'lodash';
 
 const getIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(spacesCount * depth - 2);
 
-const getContent = (value, depth) => {
+function getContent(value, depth) {
   if (!_.isObject(value)) {
     return value;
   }
   const keys = Object.keys(value);
   const result = keys.map((key) => `${getIndent(depth + 1)}  ${key}: ${getContent(value[key], depth + 1)}`);
   return `{\n${result.join('\n')}\n${getIndent(depth)}  }`;
-};
+}
+
+function getSign(type) {
+  switch (type) {
+    case 'added':
+      return '+';
+    case 'deleted':
+      return '-';
+    case 'different value':
+      return ['-', '+'];
+    default:
+      return ' ';
+  }
+}
+
+function createChildString(element, depth) {
+  const {
+    key, value, value1, value2, type,
+  } = element;
+
+  const sign = getSign(type);
+
+  if (type === 'different value') {
+    return `\n${getIndent(depth)}${sign[0]} ${key}: ${getContent(value1, depth)}\n${getIndent(depth)}${sign[1]} ${key}: ${getContent(value2, depth)}`;
+  }
+
+  return `\n${getIndent(depth)}${sign} ${key}: ${getContent(value, depth)}`;
+}
+
+function createParentString(element, depth, iter) {
+  const {
+    key, value, value1, value2, type,
+  } = element;
+
+  const sign = getSign(type);
+
+  if (type === 'different value') {
+    if (Array.isArray(value1)) {
+      return `\n${getIndent(depth)}${sign[0]} ${key}: {${iter(value1, (depth + 1))}\n${getIndent(depth)}  }\n${getIndent(depth)}${sign[1]} ${key}: ${value2}`;
+    }
+    return `\n${getIndent(depth)}${sign[0]} ${key}: ${value1}\n${getIndent(depth)}${sign[1]} ${key}: {${iter(value2, (depth + 1))}\n${getIndent(depth)}  }`;
+  }
+  return `\n${getIndent(depth)}${sign} ${key}: {${iter(value, (depth + 1))}\n${getIndent(depth)}  }`;
+}
 
 export default function getStylish(diff) {
   const iter = (data, depth) => {
     const result = data.map((element) => {
-      const {
-        key, value, value1, value2, isParent, type,
-      } = element;
-
-      if (isParent) {
-        switch (type) {
-          case 'added':
-            return `\n${getIndent(depth)}+ ${key}: {${iter(value, (depth + 1))}\n${getIndent(depth)}}`;
-          case 'deleted':
-            return `\n${getIndent(depth)}- ${key}: {${iter(value, (depth + 1))}\n${getIndent(depth)}}`;
-          case 'different value':
-            if (Array.isArray(value1)) {
-              return `\n${getIndent(depth)}- ${key}: {${iter(value1, (depth + 1))}\n${getIndent(depth)}  }\n${getIndent(depth)}+ ${key}: ${value2}`;
-            }
-            return `\n${getIndent(depth)}- ${key}: ${value1}\n${getIndent(depth)}+ ${key}: {${iter(value2, (depth + 1))}\n${getIndent(depth)}  }`;
-          default:
-            return `\n${getIndent(depth)}  ${key}: {${iter(value, (depth + 1))}\n${getIndent(depth)}  }`;
-        }
+      if (element.isParent) {
+        return createParentString(element, depth, iter);
       }
-      switch (type) {
-        case 'added':
-          return `\n${getIndent(depth)}+ ${key}: ${getContent(value, depth)}`;
-        case 'deleted':
-          return `\n${getIndent(depth)}- ${key}: ${getContent(value, depth)}`;
-        case 'different value':
-          return `\n${getIndent(depth)}- ${key}: ${getContent(value1, depth)}\n${getIndent(depth)}+ ${key}: ${getContent(value2, depth)}`;
-        default:
-          return `\n${getIndent(depth)}  ${key}: ${getContent(value, depth)}`;
-      }
+      return createChildString(element, depth);
     });
     return result.join('');
   };
